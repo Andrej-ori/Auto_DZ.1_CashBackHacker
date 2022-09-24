039ef19e862c8b62dc715889caca2547b272b765
Andrej-ori <a31338805@gmail.com>
2022-09-24 15:41:50 +0300

    modified: CashbackHackServiceTest.java :Adding to the @Asserts Fully Qualified Name org.testng.Assert annotation; Adding to the @Test Fully Qualified Name org.testng.annotations.Test annotation

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,66 +1,66 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+ // import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+ // import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
-    @Test
+    @org.testng.annotations.Test
         // меньше нуля на 1
     public void lessThanZeroBy1() {
-        assertThrows(RuntimeException.class, () ->{
+        org.testng.Assert.assertThrows(RuntimeException.class, () ->{
             service.remain(-1);
         });
     }
 
-    @Test
+    @org.testng.annotations.Test
         // равен нулю
     public void equalsZero() {
-        assertEquals(service.remain(0), 1000);
+        org.testng.Assert.assertEquals(service.remain(0), 1000);
     }
 
-    @Test
+    @org.testng.annotations.Test
         // больше нуля на 1
     public void greaterThanZeroBy1() {
-        assertEquals(service.remain(1), 999);
+        org.testng.Assert.assertEquals(service.remain(1), 999);
     }
 
-    @Test
+    @org.testng.annotations.Test
         // меньше лимита
     public void lessThanTheLimit() {
-        assertEquals(service.remain(300), 700);
+        org.testng.Assert.assertEquals(service.remain(300), 700);
     }
 
-    @Test
+    @org.testng.annotations.Test
         // меньше лимита на 1
     public void lessThanTheLimitBy1() {
-        assertEquals(service.remain(999), 1);
+        org.testng.Assert.assertEquals(service.remain(999), 1);
     }
 
-    @Test
+    @org.testng.annotations.Test
         // равно лимиту
     public void equalToTheLimit() {
-        assertEquals(service.remain(1000), 0);
+        org.testng.Assert.assertEquals(service.remain(1000), 0);
     }
 
-    @Test
+    @org.testng.annotations.Test
         // больше лимита на 1
     public void moreThanTheLimitBy1() {
-        assertEquals(service.remain(1001), 999);
+        org.testng.Assert.assertEquals(service.remain(1001), 999);
     }
 
-    @Test
+    @org.testng.annotations.Test
         // больше лимита
     public void overLimit() {
-        assertEquals(service.remain(1400), 600);
+        org.testng.Assert.assertEquals(service.remain(1400), 600);
     }
 
-    @Test
+    @org.testng.annotations.Test
         // кратно лимиту
     public void multipleOfTheLimit() {
-        assertEquals(service.remain(3000), 0);
+        org.testng.Assert.assertEquals(service.remain(3000), 0);
     }
 }
